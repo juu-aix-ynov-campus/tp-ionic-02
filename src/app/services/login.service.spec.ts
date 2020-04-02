df3ff84b5dff1a0cc2db472c1f6ac91b0320cdca
Julien Catania <julien.catania@gmail.com>
2020-04-02 14:41:15 +0200

    TU LoginService using HttpClientTestingModule

--- src/app/services/login.service.spec.ts
+++ src/app/services/login.service.spec.ts
@@ -1,12 +1,28 @@
 import {TestBed} from '@angular/core/testing';
 
 import {LoginService} from './login.service';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import User from '../models/User';
 
 describe('LoginService', () => {
-    beforeEach(() => TestBed.configureTestingModule({
-        imports: [HttpClientModule]
-    }));
+
+    let httpTestingController: HttpTestingController;
+    const mockData = [
+        {
+            id: '1',
+            name: 'Le nom de Bret',
+            username: 'Bret',
+            email: 'Sincere@april.biz'
+        }
+    ];
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule]
+        });
+
+        httpTestingController = TestBed.get(HttpTestingController);
+    });
 
     it('should be created', () => {
         const service: LoginService = TestBed.get(LoginService);
@@ -15,11 +31,23 @@ describe('LoginService', () => {
 
     it('login', () => {
         const service: LoginService = TestBed.get(LoginService);
-        const idTest = 'Bret';
-        const nomReturned = 'Leanne Graham';
+        const idTest = 'Plop';
+        let userReturned: User;
+
         service.login(idTest, 'replop')
             .subscribe(user => {
-                expect(user.nom).toBe(nomReturned);
+                userReturned = user;
             });
+
+        // attente de la requete
+        const req = httpTestingController.expectOne('https://jsonplaceholder.typicode.com/users?username=' + idTest);
+        // reponse a la requete
+        req.flush(mockData);
+        // verification que toutes les requetes soient terminees
+        httpTestingController.verify();
+
+        expect(userReturned.nom).toBe(mockData[0].name);
+        expect(userReturned.id).toBe(mockData[0].id);
+        expect(userReturned.email).toBe(mockData[0].email);
     });
 });
