c84ee8c1a09a70f49f517aebb95af604354cab25
Julien Catania <julien.catania@gmail.com>
2020-04-02 12:14:13 +0200

    homeComponent and test ok

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import {LoginService} from '../services/login.service';
-import User from '../models/User';
 
 @Component({
   selector: 'app-home',
@@ -10,14 +8,11 @@ import User from '../models/User';
 export class HomePage {
 
   title: string;
-  private user: User;
 
-  constructor(private loginService: LoginService) {}
+  constructor() {}
 
   updateTitle() {
     this.title = 'Mon Nouveau Titre';
-    this.user = this.loginService.login('AZEAZEAZE', 'P@ssw0rd');
-    console.log(this.user);
   }
 
   maNouvelleMethodeQuiNeSeraPasTestee() {
