5304c616a0a2ed451c5e1584ef713b426d9fa0b2
Julien Catania <julien.catania@gmail.com>
2020-04-02 10:16:43 +0200

    ajout appel JsonPlaceholder pour service Login

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import User from '../models/User';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   login(id: string, mdp: string): User {
+    this.http.get('https://jsonplaceholder.typicode.com/users')
+        .subscribe(value => {
+      console.log('retour WS', value);
+        });
+
+    console.log('return');
     return new User(id, 'Nom', 'Prénom', 'email@ynov.com');
   }
 }
