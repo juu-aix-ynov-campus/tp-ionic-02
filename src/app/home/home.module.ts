49cc116849c7694dbc60c9b97a6bef336983632a
Julien Catania <julien.catania@gmail.com>
2020-04-09 14:59:03 +0200

    correction singleton LoginService

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
-import {LoginService} from '../services/login.service';
 import { Camera } from '@ionic-native/camera/ngx';
 
 @NgModule({
@@ -21,6 +20,6 @@ import { Camera } from '@ionic-native/camera/ngx';
     ])
   ],
   declarations: [HomePage],
-  providers: [LoginService, Camera]
+  providers: [Camera]
 })
 export class HomePageModule {}
